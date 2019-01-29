8c13cd08fe051048f208f82a0514e7eae35b2fbd
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2019-01-29 23:16:30 +0700

    QueryActivity substituted with a SlideUpPanel. QueryActivity now unused. DownloadActivity and QueryActivity now use NoActionBar. Text stylings with Segoe UI and Montserrat. Redesigned DownloadDialog. Moved YoutubeQueryTask to separate file. Redesigned DownloadAdapter ViewHolders to use MaterialCardViews and stroke color indicators (elevation = 0). Incorrectly styled buttons (referred to default) has been fixed to use TextButton style when appropriate.
    Old fonts are deleted.
    New fonts are added.
    
    29/1/2019

--- app/src/main/java/com/yearzero/renebeats/Activities/QueryActivity.java
+++ app/src/main/java/com/yearzero/renebeats/Activities/QueryActivity.java
@@ -2,27 +2,22 @@ package com.yearzero.renebeats.Activities;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
 import com.yearzero.renebeats.Adapters.QueryAdapter;
 import com.yearzero.renebeats.Commons;
 import com.yearzero.renebeats.Query;
 import com.yearzero.renebeats.R;
+import com.yearzero.renebeats.YoutubeQueryTask;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -33,7 +28,7 @@ public class QueryActivity extends AppCompatActivity implements SwipeRefreshLayo
 
     private static final String TAG = "QueryActivity";
 
-    private SwipeRefreshLayout Swipe;
+//    private SwipeRefreshLayout Swipe;
     private RecyclerView List;
     private ImageView OfflineImg;
     private TextView OfflineMsg;
@@ -59,7 +54,7 @@ public class QueryActivity extends AppCompatActivity implements SwipeRefreshLayo
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) actionBar.setDisplayShowTitleEnabled(false);
 
-        Swipe = findViewById(R.id.swipe);
+//        Swipe = findViewById(R.id.swipe);
         List = findViewById(R.id.list);
 
         OfflineImg = findViewById(R.id.offline_img);
@@ -72,12 +67,12 @@ public class QueryActivity extends AppCompatActivity implements SwipeRefreshLayo
         List.setAdapter(adapter);
 
         OfflineAction.setOnClickListener(v -> {
-            Swipe.setRefreshing(true);
+//            Swipe.setRefreshing(true);
             onRefresh();
         });
-
-        Swipe.setOnRefreshListener(this);
-        Swipe.setRefreshing(true);
+//
+//        Swipe.setOnRefreshListener(this);
+//        Swipe.setRefreshing(true);
         onRefresh();
 
     }
@@ -95,7 +90,7 @@ public class QueryActivity extends AppCompatActivity implements SwipeRefreshLayo
                     OfflineMsg.setText("It seems that we can't connect to YouTube. Please check your connection and try again later.");
                     OfflineAction.setText("Retry");
                     OfflineAction.setOnClickListener(v -> {
-                        Swipe.setRefreshing(true);
+//                        Swipe.setRefreshing(true);
                         onRefresh();
                     });
                 } else if (results.size() <= 0) {
@@ -111,7 +106,7 @@ public class QueryActivity extends AppCompatActivity implements SwipeRefreshLayo
                 OfflineMsg.setVisibility(visi);
                 OfflineAction.setVisibility(visi);
 
-                Swipe.setRefreshing(false);
+//                Swipe.setRefreshing(false);
             }
 
             @Override
@@ -123,7 +118,7 @@ public class QueryActivity extends AppCompatActivity implements SwipeRefreshLayo
                 OfflineAction.setVisibility(View.VISIBLE);
                 OfflineAction.setText("Retry");
                 OfflineAction.setOnClickListener(v -> {
-                    Swipe.setRefreshing(true);
+//                    Swipe.setRefreshing(true);
                     onRefresh();
                 });
             }
@@ -137,72 +132,6 @@ public class QueryActivity extends AppCompatActivity implements SwipeRefreshLayo
         if (item.getItemId() == android.R.id.home) onBackPressed();
         else return super.onOptionsItemSelected(item);
         return true;
-    }
-
-    public static class YoutubeQueryTask extends AsyncTask<String, Void, List<SearchResult>> {
-
-        public interface Callbacks {
-            void onComplete(List<SearchResult> results);
-            void onTimeout();
-        }
-
-        private Callbacks listener;
-        private String pkgName;
-        private int timeout;
-
-        public YoutubeQueryTask(Callbacks listener, @NonNull String pkgName) {
-            this.listener = listener;
-            this.pkgName = pkgName;
-        }
-
-        public YoutubeQueryTask setTimeout(int timeout){
-            this.timeout = timeout;
-            return this;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            new CountDownTimer(timeout, timeout) {
-                @Override
-                public void onTick(long l) {}
-
-                @Override
-                public void onFinish() {
-                    if (getStatus() == Status.RUNNING) {
-                        cancel();
-                        if (listener != null) listener.onTimeout();
-                    }
-                }
-            }.start();
-            super.onPreExecute();
-        }
-
-        @Override
-        protected List<SearchResult> doInBackground(String... query) {
-            try {
-                YouTube youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), request -> {})
-                    .setApplicationName(pkgName)
-                    .build();
-
-                YouTube.Search.List search = youtube.search().list("id,snippet");
-                search.setKey(Commons.YT_API_KEY);
-                search.setQ(query[0]);
-
-                search.setType("video");
-                search.setMaxResults((long) Commons.Pref.query_amount);
-
-                SearchListResponse searchResponse = search.execute();
-                return searchResponse.getItems();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(List<SearchResult> results) {
-            if (listener != null) listener.onComplete(results);
-        }
     }
 
 }
